43d80967a34f065fbe851f93d0b1035ab0fd9266
terkwood <38859656+Terkwood@users.noreply.github.com>
2023-11-08 14:13:08 -0500

    fmt: example

--- example.ts
+++ example.ts
@@ -1,12 +1,11 @@
-import { trampoline } from 'https://raw.githubusercontent.com/Terkwood/trampoline.ts/1.0.0/trampoline.ts';
+import { trampoline } from "https://raw.githubusercontent.com/Terkwood/trampoline.ts/1.0.0/trampoline.ts";
 
-const helper = (num: number, acc = 1) => { 
-    return num === 0 ? acc : () => helper(num - 1, acc * num); 
-  }; 
-    
-  
-  const factorial = trampoline(helper);
-  
-  for (let i = 0; i <= 11; i++) {
-    console.log(factorial(i));
-  }
+const helper = (num: number, acc = 1) => {
+  return num === 0 ? acc : () => helper(num - 1, acc * num);
+};
+
+const factorial = trampoline(helper);
+
+for (let i = 0; i <= 11; i++) {
+  console.log(factorial(i));
+}
